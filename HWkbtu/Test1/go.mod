beb926c550055801ed23a0afd747830bc35ef42e
Adilbek <zhumazhanov40@gmail.com>
2023-01-30 17:56:28 +0600

    Fourth commit

--- HWkbtu/Test1/go.mod
+++ HWkbtu/Test1/go.mod
@@ -1,3 +1,5 @@
 module testmod
 
 go 1.19
+
+require github.com/deelbak/GoLand/HWkbtu/mathmode v0.0.0-20230128093045-9c73eb8ab8ad // indirect
--- HWkbtu/Test1/main.go
+++ HWkbtu/Test1/main.go
@@ -1,7 +1,8 @@
 package main
 import (
-
+"fmt"
+"github.com/deelbak/GoLand/HWkbtu/mathmode"
 )
 func main() {
-
+	fmt.Println(mathmode.Fac(8))
 }
