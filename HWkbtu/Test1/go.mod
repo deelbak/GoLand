ef970a7ba650d60cf7f00b008240d30f5ef2545e
Adilbek <zhumazhanov40@gmail.com>
2023-01-30 18:01:31 +0600

    Sixth commit

--- HWkbtu/Test1/go.mod
+++
@@ -1,5 +0,0 @@
-module testmod
-
-go 1.19
-
-require github.com/deelbak/GoLand/HWkbtu/mathmode v0.0.0-20230128093045-9c73eb8ab8ad // indirect
--- HWkbtu/Test1/main.go
+++
@@ -1,8 +0,0 @@
-package main
-import (
-"fmt"
-"github.com/deelbak/GoLand/HWkbtu/mathmode"
-)
-func main() {
-	fmt.Println(mathmode.Fac(8))
-}
